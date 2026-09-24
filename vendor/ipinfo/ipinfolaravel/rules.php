68e33d21fd6aa2335ee6203744790461ff5f8fbf
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2026-09-24 16:01:59 +0300

    chore: remove vendor directory from git

--- vendor/ipinfo/ipinfolaravel/rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: ipinfo, ipinfolaravel, ipinfo, ipinfolaravel
-return [
-    'src/MyPackage.php' => 'src/ipinfolaravel.php',
-    'config/mypackage.php' => 'config/ipinfolaravel.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/ipinfolaravel.php',
-    'src/MyPackageServiceProvider.php' => 'src/ipinfolaravelServiceProvider.php',
-];
